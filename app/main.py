eb2e8a11a65ffa278c6ee10bbd0d73bf5e9287f2
Enrico Gandini <enricogandini93@gmail.com>
2023-10-01 21:39:22 +0200

    feat: wip, start adding models

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import fastapi
+from models import Exercise
 
 app = fastapi.FastAPI()
 
@@ -7,3 +8,15 @@ app = fastapi.FastAPI()
 def hello() -> dict[str, str]:
     result = {"message": "Hello, World!"}
     return result
+
+
+@app.get("/exercises")
+def get_exercises() -> list[Exercise]:
+    """Get all the available exercises"""
+    return [
+        Exercise(key="BP", name="Bench Press"),
+        Exercise(key="DL", name="Deadlift"),
+        Exercise(key="SQ", name="Squat"),
+        Exercise(key="VP", name="Vertical Press"),
+        Exercise(key="BC", name="Biceps Curl"),
+    ]
--- app/models.py
+++ app/models.py
@@ -10,17 +10,36 @@ class Exercise(BaseModel):
 
 
 class SetOfExcercise(BaseModel):
-    exercise_key: str
     weight: float
-    repetitions: int
+    n_repetitions: int
     notes: str | None = None
+
+    @property
+    def volume(self) -> float:
+        return self.weight * self.n_repetitions
 
 
 class SessionOfExercise(BaseModel):
     exercise_key: str
     sets: list[SetOfExcercise]
 
+    @property
+    def n_sets(self) -> int:
+        return len(self.sets)
+
+    @property
+    def volume(self) -> float:
+        return sum(set.volume for set in self.sets)
+
+    @property
+    def n_repetitions(self) -> int:
+        return sum(set.n_repetitions for set in self.sets)
+
 
 class TrainingSession(BaseModel):
     date: datetime.date
     excercises: list[SessionOfExercise]
+
+    @property
+    def n_excercises(self) -> int:
+        return len(self.excercises)
