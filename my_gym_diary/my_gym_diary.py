118cb4039c80d85d923c198a4ade326311f0471a
Enrico Gandini <enricogandini93@gmail.com>
2023-10-29 19:47:59 +0100

    feat: use simplest reflex app as base
    From: https://www.youtube.com/watch?v=3fWJYkjmaLU

--- my_gym_diary/my_gym_diary.py
+++ my_gym_diary/my_gym_diary.py
@@ -1,12 +1,24 @@
-"""Welcome to Reflex!."""
-
-from my_gym_diary import styles
-
-# Import all the pages.
-from my_gym_diary.pages import *
+"""Welcome to Reflex!"""
 
 import reflex as rx
 
-# Create the app and compile it.
-app = rx.App(style=styles.base_style)
+
+class State(rx.State):
+    """The app state"""
+
+    count = 0
+
+    def increment(self):
+        self.count += 1
+
+
+def index() -> rx.Component:
+    return rx.vstack(
+        rx.heading(State.count),
+        rx.button("Increment", on_click=State.increment),
+    )
+
+
+app = rx.App(state=State)
+app.add_page(index)
 app.compile()
