43613d4e67b8c019216384bc2533bc64ce792155
Enrico Gandini <enricogandini93@gmail.com>
2023-11-02 12:46:06 +0100

    add enter exercise

--- my_gym_diary/my_gym_diary.py
+++ my_gym_diary/my_gym_diary.py
@@ -1,7 +1,8 @@
 """Welcome to Reflex!"""
 
-import reflex as rx
 import asyncio
+
+import reflex as rx
 
 from . import models
 
@@ -10,9 +11,6 @@ class State(rx.State):
     """The app state"""
 
     available_exercises: list[models.ExerciseType]
-
-    # TODO: create button that adds new exercise
-    new_exercise: models.ExerciseType
 
     def _get_all_exercises(self):
         with rx.session() as session:
@@ -28,10 +26,41 @@ class State(rx.State):
             await asyncio.sleep(1)
             async with self:
                 self.available_exercises = self._get_all_exercises()
-    
+
     @rx.var
     def n_available_exercises(self) -> int:
         return len(self.available_exercises)
+
+
+class NewExerciseFormState(State):
+    form_data: dict[str, str]
+
+    def _save_exercise(self):
+        exercise = models.ExerciseType(
+            code=self.form_data["code"],
+            name=self.form_data["name"],
+        )
+        with rx.session() as session:
+            session.add(exercise)
+            session.commit()
+
+    def handle_submit(self, form_data: dict[str, str]):
+        self.form_data = form_data
+        self._save_exercise()
+        self.form_data = {}
+
+
+def new_exercise_form():
+    return rx.vstack(
+        rx.form(
+            rx.vstack(
+                rx.input(placeholder="Code", id="code"),
+                rx.input(placeholder="Name", id="name"),
+                rx.button("Create Exercise", type_="submit"),
+            ),
+            on_submit=NewExerciseFormState.handle_submit,
+        ),
+    )
 
 
 def available_exercises_display():
@@ -42,6 +71,7 @@ def available_exercises_display():
         width="30vw",
         height="100%",
     )
+
 
 def render_exercise_type(exercise: models.ExerciseType):
     return rx.hstack(
@@ -55,6 +85,7 @@ def render_exercise_type(exercise: models.ExerciseType):
 
 def index() -> rx.Component:
     return rx.hstack(
+        new_exercise_form(),
         rx.spacer(),
         available_exercises_display(),
         rx.spacer(),
