4b957ad009410dd2b202ec3a514efa5d6758eacd
Enrico Gandini <enricogandini93@gmail.com>
2023-11-19 13:06:54 +0100

    feat: add Exercise model

--- workouts/models.py
+++ workouts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Exercise(models.Model):
+    code = models.CharField(max_length=5, unique=True)
+    name = models.CharField(max_length=25, unique=True)
+    description = models.CharField(max_length=100, null=True, blank=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(models.functions.Lower("code"), name="unique_code"),
+            models.UniqueConstraint(models.functions.Lower("name"), name="unique_name"),
+            models.UniqueConstraint(fields=["code", "name"], name="unique_exercise"),
+        ]
