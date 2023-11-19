f2013e3f2b0605253bf69279069f2c640099e8bf
Enrico Gandini <enricogandini93@gmail.com>
2023-11-19 14:34:26 +0100

    feat: improve Exercise admin

--- workouts/admin.py
+++ workouts/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Exercise
 
-admin.site.register(Exercise)
+
+@admin.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = ("code", "name", "description")
+    search_fields = ("code", "name", "description")
