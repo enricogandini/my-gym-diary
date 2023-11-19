2210bf0456d7af12fd474d6ceb73c66e9946996b
Enrico Gandini <enricogandini93@gmail.com>
2023-11-19 14:02:30 +0100

    feat: add validation for code and name

--- workouts/models.py
+++ workouts/models.py
@@ -1,9 +1,19 @@
 from django.db import models
+from django.core.validators import RegexValidator
+
+validator_only_letters = RegexValidator(r"^[a-zA-Z]*$", "Only letters are allowed.")
+validator_only_letters_and_spaces = RegexValidator(
+    r"^[a-zA-Z ]*$", "Only letters and spaces are allowed."
+)
 
 
 class Exercise(models.Model):
-    code = models.CharField(max_length=5, unique=True)
-    name = models.CharField(max_length=25, unique=True)
+    code = models.CharField(
+        max_length=5, unique=True, validators=[validator_only_letters]
+    )
+    name = models.CharField(
+        max_length=25, unique=True, validators=[validator_only_letters_and_spaces]
+    )
     description = models.TextField(max_length=1000, null=True, blank=True)
 
     class Meta:
