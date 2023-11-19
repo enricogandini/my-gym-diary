8531382ad091132b2ebccf672a60efb63491f096
Enrico Gandini <enricogandini93@gmail.com>
2023-11-19 13:56:02 +0100

    feat: increase description size

--- workouts/models.py
+++ workouts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Exercise(models.Model):
     code = models.CharField(max_length=5, unique=True)
     name = models.CharField(max_length=25, unique=True)
-    description = models.CharField(max_length=100, null=True, blank=True)
+    description = models.TextField(max_length=1000, null=True, blank=True)
 
     class Meta:
         constraints = [
