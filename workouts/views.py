a6d82bcb5a8b92e1d3e179e15a44427c3f276c26
Enrico Gandini <enricogandini93@gmail.com>
2023-11-20 21:13:41 +0100

    feat: add some information to Workouts index
    TODO: this was just an experiment to understand views. Use a proper template!

--- workouts/views.py
+++ workouts/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
 
+from .models import Exercise
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the workouts index.")
+    message = "Hello, world. You're at the workouts index."
+    exercises = Exercise.objects.all()
+    message += f"<br>There are {exercises.count()} exercises."
+    for exercise in exercises:
+        message += f"<br>{exercise}"
+    return HttpResponse(message)
