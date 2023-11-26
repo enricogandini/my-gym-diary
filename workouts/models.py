b86eb29342f757fd9d4beb15008a0359700abc60
Enrico Gandini <enricogandini93@gmail.com>
2023-11-26 12:14:20 +0100

    fix: improve exercise name validator
    Now no trailing spaces are allowed

--- workouts/models.py
+++ workouts/models.py
@@ -3,20 +3,21 @@ from decimal import Decimal
 from django.db import models
 from django.core.validators import RegexValidator
 
-validator_only_letters = RegexValidator(
-    r"^[a-zA-Z]*$", message="Only letters are allowed."
+validator_only_latin_letters = RegexValidator(
+    r"^[a-zA-Z]*$", message="Only latin letters are allowed."
 )
-validator_only_letters_and_spaces = RegexValidator(
-    r"^[a-zA-Z ]*$", message="Only letters and spaces are allowed."
+validator_latin_words_single_spaces = RegexValidator(
+    r"^[a-zA-Z]+(?: [a-zA-Z]+)*$",
+    message="Only latin letters and single spaces are allowed, no trailing spaces.",
 )
 
 
 class Exercise(models.Model):
     code = models.CharField(
-        max_length=5, unique=True, validators=[validator_only_letters]
+        max_length=5, unique=True, validators=[validator_only_latin_letters]
     )
     name = models.CharField(
-        max_length=25, unique=True, validators=[validator_only_letters_and_spaces]
+        max_length=25, unique=True, validators=[validator_latin_words_single_spaces]
     )
     description = models.TextField(max_length=1000, null=True, blank=True)
 
