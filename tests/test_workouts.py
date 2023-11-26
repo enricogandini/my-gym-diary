e68d9679d11226ccb364126d164e3536b42248e4
Enrico Gandini <enricogandini93@gmail.com>
2023-11-26 11:48:04 +0100

    tests: add tests for invalid code and names
    tests fail for invalid names! fix them!

--- tests/test_workouts.py
+++ tests/test_workouts.py
@@ -14,6 +14,7 @@ def workout_empty(db):
     workout.delete()
 
 
+max_length_code = Exercise._meta.get_field("code").max_length
 invalid_exercise_codes = [
     "",
     " ",
@@ -22,11 +23,29 @@ invalid_exercise_codes = [
     "a a",
     "a1",
     "1a",
+    "a" * (max_length_code + 1),
 ]
 
 
 @pytest.mark.parametrize("code", invalid_exercise_codes)
 def test_exercise_invalid_code(db, code):
     exercise = Exercise(code=code, name="ciao")
+    with pytest.raises(ValidationError):
+        exercise.clean_fields()
+
+
+max_length_name = Exercise._meta.get_field("name").max_length
+invalid_exercise_names = [
+    "",
+    " ",
+    " a",
+    "a ",
+    "a" * (max_length_name + 1),
+]
+
+
+@pytest.mark.parametrize("name", invalid_exercise_names)
+def test_exercise_invalid_name(db, name):
+    exercise = Exercise(code="ciao", name=name)
     with pytest.raises(ValidationError):
         exercise.clean_fields()
