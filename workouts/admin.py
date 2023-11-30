39ed3d2a334444a4d728d4c03e7a74129ab7e9f5
Enrico Gandini <enricogandini93@gmail.com>
2023-11-30 23:08:10 +0100

    feat: improve setofexercise admin

--- workouts/admin.py
+++ workouts/admin.py
@@ -11,8 +11,8 @@ class ExerciseAdmin(admin.ModelAdmin):
 
 @admin.register(SetOfExercise)
 class SetOfExerciseAdmin(admin.ModelAdmin):
-    list_display = ("exercise", "n_repetitions", "weight", "notes")
-    search_fields = ("exercise", "n_repetitions", "weight", "notes")
+    list_display = ("workout", "exercise", "n_repetitions", "weight", "notes")
+    search_fields = ("notes", "exercise__code", "exercise__name", "workout__date")
 
 
 admin.site.register(Workout)
