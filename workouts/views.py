59d746cb6ac3f304c9f4627f03f260eab519c234
Enrico Gandini <enricogandini93@gmail.com>
2023-11-30 19:53:13 +0100

    refactor: no need to define separate variable for exercises

--- workouts/views.py
+++ workouts/views.py
@@ -9,6 +9,7 @@ def index(request):
     start_date, end_date = get_start_end_dates_from_period(
         datetime.date.today(), "month"
     )
-    context = Exercise.objects.compute_report(start_date, end_date)
-    context = {"exercises": list(context)}
+    context = {
+        "exercises": Exercise.objects.compute_report(start_date, end_date),
+    }
     return render(request, "workouts/index.html", context)
