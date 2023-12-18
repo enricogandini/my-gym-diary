8939cc569075f3ef61ebf29fb587123e97d91c09
Enrico Gandini <enricogandini93@gmail.com>
2023-12-18 08:23:18 +0100

    feat: wip, add chart
    Based on:
    https://www.advantch.com/blog/how-to-add-charts-to-your-python-application/
    
    TODO: substitute hardcoded data with retrieved workouts data in view! Adapt the charts!
    
    Look at the official pyecharts documentation:
    https://pyecharts.org/#/en-us/web_django
    
    Also look at the Sanic documentation, which presents a simpler HTML file:
    https://pyecharts.org/#/en-us/web_sanic

--- workouts/views.py
+++ workouts/views.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.shortcuts import render
 
+from .charts import Colors, line_chart
 from .models import Exercise, Workout, get_start_end_dates_from_period
 
 
@@ -21,3 +22,31 @@ def index(request):
         "end_date": end_date,
     }
     return render(request, "workouts/index.html", context)
+
+
+def dashboard_view(request):
+    total_page_views = {
+        "x": ["mon", "tue", "wed", "thur", "fri", "sat", "sun"],
+        "y": [8, 20, 15, 20, 50, 30, 35],
+        "chart_title": "Total Page Views",
+    }
+
+    unique_visitors = {
+        "x": ["mon", "tue", "wed", "thur", "fri", "sat", "sun"],
+        "y": [3, 4, 10, 12, 30, 20, 33],
+        "chart_title": "Unique Visitors",
+    }
+    signups = {
+        "x": ["mon", "tue", "wed", "thur", "fri", "sat", "sun"],
+        "y": [3, 4, 10, 12, 30, 20, 33],
+        "chart_title": "Signups",
+    }
+    charts = [
+        line_chart(total_page_views),
+        line_chart(unique_visitors, Colors.yellow),
+        line_chart(signups, Colors.green),
+    ]
+
+    context = {"charts": charts}
+
+    return render(request, "workouts/dashboard.html", context)
