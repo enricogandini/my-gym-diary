69397c8f4038b34941b2c2b7d5213093bdcbcd24
Enrico Gandini <enricogandini93@gmail.com>
2023-12-22 11:22:20 +0100

    feat: use calendar selector for date range form.
    NOTE: the calendar uses the default HTML date input widget, and its date format cannot be changed! It is useless to set format, input_format...
    See: https://forum.djangoproject.com/t/dateinput-format-not-changing-from-default/10009/6
    
    If I wanted to use a proper customizable date picker, I should implement a widget which uses Javascript, see:
    https://simpleisbetterthancomplex.com/tutorial/2019/01/03/how-to-use-date-picker-with-django.html
    
    I could also try to use the django admin widget for dates

--- workouts/forms.py
+++ workouts/forms.py
@@ -2,5 +2,9 @@ from django import forms
 
 
 class DateRangeForm(forms.Form):
-    start_date = forms.DateField(label="Start Date")
-    end_date = forms.DateField(label="End Date")
+    start_date = forms.DateField(
+        label="Start Date", widget=forms.DateInput(attrs={"type": "date"})
+    )
+    end_date = forms.DateField(
+        label="End Date", widget=forms.DateInput(attrs={"type": "date"})
+    )
