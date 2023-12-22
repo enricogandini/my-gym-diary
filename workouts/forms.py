c3e628efa5900a6bccf08b30d7ec227c9faecb95
Enrico Gandini <enricogandini93@gmail.com>
2023-12-22 18:32:28 +0100

    feat: use better date picker with custom date formatting

--- workouts/forms.py
+++ workouts/forms.py
@@ -1,10 +1,14 @@
 from django import forms
+from django_flatpickr.schemas import FlatpickrOptions
+from django_flatpickr.widgets import DatePickerInput
 
 
 class DateRangeForm(forms.Form):
     start_date = forms.DateField(
-        label="Start Date", widget=forms.DateInput(attrs={"type": "date"})
+        label="Start Date",
+        widget=DatePickerInput(options=FlatpickrOptions(altFormat="Y-m-d")),
     )
     end_date = forms.DateField(
-        label="End Date", widget=forms.DateInput(attrs={"type": "date"})
+        label="End Date",
+        widget=DatePickerInput(options=FlatpickrOptions(altFormat="Y-m-d")),
     )
