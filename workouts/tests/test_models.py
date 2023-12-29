bcddaf42270cd251217549f37bd55b6880d12be5
Enrico Gandini <enricogandini93@gmail.com>
2023-12-29 17:09:27 +0100

    test: wip, test computed report

--- workouts/tests/test_models.py
+++ workouts/tests/test_models.py
@@ -188,3 +188,38 @@ def test_load_correct_excel(db, file):
     assert ids_retrieved_sets == set(created_objects["set_of_exercise"])
     assert len(created_objects["workout"]) == df["Date"].nunique()
     assert len(created_objects["exercise"]) == df["Exercise"].nunique()
+
+
+@pytest.mark.parametrize("file", _CORRECT_EXCEL_FILES)
+def test_compute_report_total_per_exercise(db, file):
+    df = (
+        pd.read_excel(file)
+        .assign(volume=lambda df_: df_["Reps"] * df_["Weight"])
+        .rename(columns={"Exercise": "code"})
+    )
+    start_date = df["Date"].min().date()
+    end_date = df["Date"].max().date()
+    SetOfExercise.objects.create_from_excel(file)
+    report = SetOfExercise.objects.compute_report(
+        start_date=start_date, end_date=end_date, periodicity="total", per_exercise=True
+    )
+    report = pd.DataFrame(report).drop(columns="name").set_index("code")
+    assert report.shape[0] == df["Exercise"].nunique()
+    expected_report = df.groupby("code").agg(
+        max_volume=pd.NamedAgg(column="volume", aggfunc="max"),
+        min_volume=pd.NamedAgg(column="volume", aggfunc="min"),
+        avg_volume=pd.NamedAgg(column="volume", aggfunc="mean"),
+        total_volume=pd.NamedAgg(column="volume", aggfunc="sum"),
+        max_weight=pd.NamedAgg(column="Weight", aggfunc="max"),
+        min_weight=pd.NamedAgg(column="Weight", aggfunc="min"),
+        avg_weight=pd.NamedAgg(column="Weight", aggfunc="mean"),
+        total_weight=pd.NamedAgg(column="Weight", aggfunc="sum"),
+        max_repetitions=pd.NamedAgg(column="Reps", aggfunc="max"),
+        min_repetitions=pd.NamedAgg(column="Reps", aggfunc="min"),
+        avg_repetitions=pd.NamedAgg(column="Reps", aggfunc="mean"),
+        total_repetitions=pd.NamedAgg(column="Reps", aggfunc="sum"),
+        n_workouts=pd.NamedAgg(column="Date", aggfunc="nunique"),
+        n_sets=pd.NamedAgg(column="Date", aggfunc="count"),
+    )
+    assert report.equals(expected_report)
+    assert report["n_workouts"] == df["Date"].nunique()
