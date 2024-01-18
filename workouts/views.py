d6984f3328e524d65bdcab4e16aaa1a497607ba0
Enrico Gandini <enricogandini93@gmail.com>
2024-01-18 19:43:03 +0100

    fix: include user in views that compute report

--- workouts/views.py
+++ workouts/views.py
@@ -14,12 +14,21 @@ def index(request):
     start_date, end_date = get_start_end_dates_from_period(
         datetime.date.today(), "month"
     )
+    user = request.user
     context = {
-        "exercises": SetOfExercise.objects.compute_report(
-            start_date, end_date, periodicity="total", per_exercise=True
+        "exercises": SetOfExercise.objects.compute_report_for_user(
+            user=user,
+            start_date=start_date,
+            end_date=end_date,
+            periodicity="total",
+            per_exercise=True,
         ),
-        "interval_statistics": SetOfExercise.objects.compute_report(
-            start_date, end_date, periodicity="total", per_exercise=False
+        "interval_statistics": SetOfExercise.objects.compute_report_for_user(
+            user=user,
+            start_date=start_date,
+            end_date=end_date,
+            periodicity="total",
+            per_exercise=False,
         ),
         "start_date": start_date,
         "end_date": end_date,
@@ -30,13 +39,19 @@ def index(request):
 def chart(request):
     if request.method == "POST":
         form = DateRangeForm(request.POST)
+
         if form.is_valid():
             start_date = form.cleaned_data["start_date"]
             end_date = form.cleaned_data["end_date"]
+            user = request.user
 
             # Get all sets of exercises for the given period, counting the amount of sets per day
-            workouts = SetOfExercise.objects.compute_report(
-                start_date, end_date, periodicity="daily", per_exercise=False
+            workouts = SetOfExercise.objects.compute_report_for_user(
+                user=user,
+                start_date=start_date,
+                end_date=end_date,
+                periodicity="daily",
+                per_exercise=False,
             )
 
             # Prepare data for the chart
